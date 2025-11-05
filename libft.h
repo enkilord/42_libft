dc6237d6f6bee7d8123f7d9d661eeebb90955055
mahats_v <mahats_v@etna-alternance.net>
2025-11-05 13:02:22 +0100

    1st Commit bitchh git add .

---
+++ libft.h
@@ -0,0 +1,17 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+/* ************************************************************************** */
+/*                             Partie obligatoire                             */
+/* ************************************************************************** */
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int     ft_isalnum(int c);
+
+
+
+#endif
